080a4bb3eb346d52e17031c43df99898b8eb511f
tetr-git <sklose@posteo.net>
2023-03-24 17:08:22 +0100

    added security func

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const enforceSSL = require("express-enforces-ssl");
 const cookieParser = require("cookie-parser");
 const gpxRoutes = require("./routes/gpxRoutes");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const userRoutes = require("./routes/userRoutes");
+const https = require("https");
+const fs = require("fs");
 
 const app = express();
 const port = process.env.PORT;
+
+const privateKey = fs.readFileSync("./ssl/key.pem", "utf8");
+const certificate = fs.readFileSync("./ssl/cert.pem", "utf8");
+const credentials = { key: privateKey, cert: certificate };
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests, please try again later.",
+});
 
 // Middleware
 const corsOptions = {
@@ -19,10 +34,15 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
+app.use(helmet());
+app.use("/api", apiLimiter);
+app.use(enforceSSL());
 
 // Routes
 app.use("/api/gpx", gpxRoutes);
 app.use("/api", userRoutes);
+
+const httpsServer = https.createServer(credentials, app);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
@@ -30,6 +50,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Internal server error" });
 });
 
-app.listen(port, () => {
+httpsServer.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
